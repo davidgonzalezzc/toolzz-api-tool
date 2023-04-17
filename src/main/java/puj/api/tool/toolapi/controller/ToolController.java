0b2fe36a4cede9915d5ba53511d7ea1ad438850f
davidgonzalezzc <cd-gonzalez@javeriana.edu.co>
2023-04-17 18:21:18 -0500

    clean code

--- src/main/java/puj/api/tool/toolapi/controller/ToolController.java
+++ src/main/java/puj/api/tool/toolapi/controller/ToolController.java
@@ -4,31 +4,12 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 import puj.api.tool.toolapi.entity.Tool;
 import puj.api.tool.toolapi.service.ToolService;
-import puj.api.tool.toolapi.service.ToolServiceImp;
-
-//
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.logging.Logger;
-
-//
 
 
 @RestController
--- src/main/java/puj/api/tool/toolapi/entity/City.java
+++ src/main/java/puj/api/tool/toolapi/entity/City.java
@@ -3,7 +3,6 @@ package puj.api.tool.toolapi.entity;
 import java.io.Serializable;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
 import lombok.Data;
