913fff6c6c4f93ec45c19466916fca4e5948d173
davidgonzalezzc <cd-gonzalez@javeriana.edu.co>
2023-04-16 20:16:22 -0500

    init

---
+++ src/main/java/puj/api/tool/toolapi/ToolapiApplication.java
@@ -0,0 +1,13 @@
+package puj.api.tool.toolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/puj/api/tool/toolapi/ToolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package puj.api.tool.toolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
